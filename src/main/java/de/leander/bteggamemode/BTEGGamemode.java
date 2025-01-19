d14381e32fa3b37582da6f9465972e6ec05784a7
JaskerX <jaskerlp@gmx.de>
2025-01-19 18:47:50 +0100

    spectator tp requires permission

--- src/main/java/de/leander/bteggamemode/BTEGGamemode.java
+++ src/main/java/de/leander/bteggamemode/BTEGGamemode.java
@@ -3,6 +3,7 @@ package de.leander.bteggamemode;
 import de.leander.bteggamemode.commands.*;
 import de.leander.bteggamemode.events.FadeFormEvent;
 import de.leander.bteggamemode.events.JoinMessage;
+import de.leander.bteggamemode.events.PlayerTeleport;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,6 +15,7 @@ public final class BTEGGamemode extends JavaPlugin {
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new JoinMessage(), this);
         getServer().getPluginManager().registerEvents(new FadeFormEvent(), this);
+        getServer().getPluginManager().registerEvents(new PlayerTeleport(), this);
         //getServer().getPluginManager().registerEvents(new BedrockTerraBlock(), this); Bedrock can join on 1.18+
         getCommand("terraform").setExecutor(new Terraform());
         getCommand("cover").setExecutor(new CoverCommand());
